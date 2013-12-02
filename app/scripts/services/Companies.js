e6acd858e10b099e9e98d99a926ffa083f0e5330
Jonatan Dahl <jo_da12@hotmail.com>
2013-12-02 04:40:56 +0100

    some sort of circular thing with all companies

--- app/scripts/services/Companies.js
+++ app/scripts/services/Companies.js
@@ -2,21 +2,11 @@
 
 angular.module('angularD3App')
   .factory('Companies', function($q, $http) {
-    var d = $q.defer(),
-      companiesList,
-      Companies = {
-        loaded: function() {
-          return true;
-        },
-        companies: function() {
-          return companiesList;
-        }
-      };
+    var d = $q.defer();
 
     $http.get('/companies.json').then(function(data) {
       console.log('factory loaded json', data);
-      companiesList = data;
-      d.resolve(data);
+      d.resolve(data.data);
     });
 
     return d.promise;
